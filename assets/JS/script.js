6b682d63b32f60c3e2bdc9c59d8610cd8df1d44f
MarkCyg <87244359+MarkCyg@users.noreply.github.com>
2021-08-19 13:13:10 -0400

    btn probs

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,17 +2,9 @@ alert("Welcome to digital Quiz!");
 
 
 //button
+document.getElementById("btn1").addEventListener("click", function (){
+ 
+  alert("Welcome To Thunder Dome Bitch");
+  document.getElementsByClass("start").style.visiblity = 'hidden';
+});
 
-
-//timer
-i = 60;
-function onTimer() {
-  document.getElementById('mycounter').innerHTML = i;
-  i--;
-  if (i < 0) {
-    alert('Times UP!');
-  }
-  else {
-    setTimeout(onTimer, 1000);
-  }
-}
