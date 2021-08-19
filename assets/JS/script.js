54d9fb3f1a3a9430de28381e0b12db6229ed4947
MarkCyg <87244359+MarkCyg@users.noreply.github.com>
2021-08-19 14:34:43 -0400

    fight club

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,27 @@
 alert("Welcome to digital Quiz!");
 
 
-//button
-document.getElementById("btn1").addEventListener("click", function (){
- 
-  alert("Welcome To Thunder Dome Bitch");
-  document.getElementsByClass("start").style.visiblity = 'hidden';
-});
+//Variables and whatnot
+ const startButton = document.getElementById('strtbtn');
+ const answerA = document.getElementById('Abtn');
+ const answerB = document.getElementById('Bbtn');
+ const answerC = document.getElementById('Cbtn');
+ const answerT = document.getElementById('Tbtn');
+ const answerF = document.getElementById('Fbtn');
 
+ //Starting the game
+ let counter = 180;
+ let timerHTMLText = document.getElementById('timer');
+ var beginCountdown;
+
+ startButton.addEventListener('click', () => {
+   alert('Welcome to THUNDER DOME!!!')
+   alert('The  rules are simple! YOU DO NOT TALK ABOUT FIGHT CLUB!!! You are being timed... 3 minutes. If the time runs out before you are finished, you lose those points. You will be docked 20 seconds for any inccorect answer. If this is your first time here... You have to fight.')
+   startGame();
+   startTimer();
+ });
+
+ function startGame() {
+   startButton.textContent = "Let's Roll";
+
+ }
